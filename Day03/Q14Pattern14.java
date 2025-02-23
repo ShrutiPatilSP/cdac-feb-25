2deeae837c6d5a8f9e83757ff1a8642a9b92f157
ShrutiPatilSP <shrutipatil1103@gmail.com>
2025-02-23 23:07:59 +0530

    Add files via upload

---
+++ Day03/Q14Pattern14.java
@@ -0,0 +1,19 @@
+public class Q14Pattern14
+ {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+// Sample Output:
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
